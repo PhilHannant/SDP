90e1bd6cc53fb00cebf933ffd4f2893a03130f9d
philhannant <philip.a.hannant@gmail.com>
2016-01-18 18:34:21 +0000

    helloworldspringwithdixml added

---
+++ WorkSheet2/src/helloworldspringwithdixml/HelloWorldSpringWithDIXMLFileConstructorArgument.java
@@ -0,0 +1,21 @@
+package helloworldspringwithdixml;
+
+/**
+ * Created by philhannant on 18/01/2016.
+ */
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+public class HelloWorldSpringWithDIXMLFileConstructorArgument {
+    public static void main(String[] args) throws Exception {
+// get the bean factory
+        BeanFactory factory = getBeanFactory();
+
+        MessageRenderer mr = (MessageRenderer) factory.getBean("renderer");
+        mr.render();
+    }
+    private static BeanFactory getBeanFactory() throws Exception {
+// create a bean factory from beans.xml
+        BeanFactory factory = new ClassPathXmlApplicationContext("/beans.xml");
+        return factory;
+    }
+}
