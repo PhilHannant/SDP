6c6696e61ddc7f9ef635f3aa9bb3fe64dad6d169
philhannant <philip.a.hannant@gmail.com>
2016-01-18 11:07:40 +0000

    Code added

--- WorkSheet1/src/Driver.java
+++ WorkSheet1/src/Driver.java
@@ -3,6 +3,7 @@
  */
 public class Driver {
     public static void main(String[] args) {
-// YOUR CODE GOES HERE
+        Storage<BankAccount> aStorage = new Storage<>();
+        Storage<String> sStorage = new Storage<>();
     }
 }
