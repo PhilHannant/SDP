3e505ed74bf59853748e2ff1768e29d28d6a42c0
philhannant <philip.a.hannant@gmail.com>
2016-01-18 20:11:47 +0000

    configurable provider added

---
+++ WorkSheet2/src/helloworldspringwithdixml/ConfigurableMessageProvider.java
@@ -0,0 +1,14 @@
+package helloworldspringwithdixml;
+
+/**
+ * Created by philhannant on 18/01/2016.
+ */
+public class ConfigurableMessageProvider implements MessageProvider {
+    private String message;
+    public ConfigurableMessageProvider(String message) {
+        this.message = message;
+    }
+    public String getMessage() {
+        return message;
+    }
+}
