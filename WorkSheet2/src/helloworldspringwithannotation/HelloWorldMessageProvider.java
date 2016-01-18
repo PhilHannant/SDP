6b6c830195d0e719007a0d40c735fa7dc7617f5f
philhannant <philip.a.hannant@gmail.com>
2016-01-18 20:26:59 +0000

    back on track after little mistake

---
+++ WorkSheet2/src/helloworldspringwithannotation/HelloWorldMessageProvider.java
@@ -0,0 +1,12 @@
+package helloworldspringwithannotation;
+
+/**
+ * Created by philhannant on 18/01/2016.
+ */
+public class HelloWorldMessageProvider {
+    @Override
+    public String getMessage() {
+
+        return "Hello World!";
+    }
+}
