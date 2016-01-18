3f8632336b4f88cb4b02ce560407f8f4d3e709e4
philhannant <philip.a.hannant@gmail.com>
2016-01-18 18:36:54 +0000

    messageprovider and messagerenderer added

---
+++ WorkSheet2/src/helloworldspringwithdixml/MessageRenderer.java
@@ -0,0 +1,12 @@
+package helloworldspringwithdixml;
+
+/**
+ * Created by philhannant on 18/01/2016.
+ */
+public interface MessageRenderer {
+    void render();
+
+    void setMessageProvider(MessageProvider provider);
+
+    MessageProvider getMessageProvider();
+}
