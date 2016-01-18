fb5c8ecb2b60516f051a0ae5ca48ae522f6a2670
philhannant <philip.a.hannant@gmail.com>
2016-01-18 11:32:41 +0000

    Cast added, now compiles.

--- WorkSheet1/src/Driver.java
+++ WorkSheet1/src/Driver.java
@@ -5,5 +5,21 @@ public class Driver {
     public static void main(String[] args) {
         Storage<BankAccount> aStorage = new Storage<>();
         Storage<String> sStorage = new Storage<>();
+
+        Class baCls = BankAccount.class;
+        try {
+            BankAccount myAccount = (BankAccount) baCls.newInstance();
+            aStorage.setValue(myAccount);
+// Deposit
+            myAccount.deposit(15);
+        }
+        catch ( InstantiationException e ) {
+// ...
+        }
+        catch ( IllegalAccessException e ) {
+// ...
+        }
+
+
     }
 }
