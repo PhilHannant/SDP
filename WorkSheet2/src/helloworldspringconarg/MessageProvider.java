a8325bcc3dcd621352f2666432f327fa7f3d0ac5
philhannant <philip.a.hannant@gmail.com>
2016-01-18 20:06:56 +0000

    interfaces added

---
+++ WorkSheet2/src/helloworldspringconarg/MessageProvider.java
@@ -0,0 +1,8 @@
+package helloworldspringconarg;
+
+/**
+ * Created by philhannant on 18/01/2016.
+ */
+public interface MessageProvider {
+    public String getMessage();
+}
---
+++ WorkSheet2/src/helloworldspringconarg/MessageRenderer.java
@@ -0,0 +1,13 @@
+package helloworldspringconarg;
+
+/**
+ * Created by philhannant on 18/01/2016.
+ */
+public interface MessageRenderer {
+
+    void render();
+
+    void setMessageProvider(MessageProvider provider);
+
+    MessageProvider getMessageProvider();
+}
