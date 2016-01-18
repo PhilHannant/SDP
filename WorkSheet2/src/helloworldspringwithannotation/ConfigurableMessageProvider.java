c7968263ab547cd14e9c3ad80a25665c83ffcfe1
philhannant <philip.a.hannant@gmail.com>
2016-01-18 20:40:39 +0000

    All classes and xml added, run successfully

---
+++ WorkSheet2/src/helloworldspringwithannotation/ConfigurableMessageProvider.java
@@ -0,0 +1,16 @@
+package helloworldspringwithannotation;
+
+/**
+ * Created by philhannant on 18/01/2016.
+ */
+public class ConfigurableMessageProvider implements MessageProvider{
+    private String message;
+
+    public ConfigurableMessageProvider(String message) {
+        this.message = message;
+    }
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
