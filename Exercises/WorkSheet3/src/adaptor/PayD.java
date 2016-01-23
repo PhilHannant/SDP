2ff580f4f3a51396c193df0ffcbfa5a2187a6f2c
philhannant <philip.a.hannant@gmail.com>
2016-01-23 16:05:19 +0000

    corrected

---
+++ Exercises/WorkSheet3/src/adaptor/PayD.java
@@ -0,0 +1,26 @@
+package adaptor;
+
+/**
+ * Created by philhannant on 23/01/2016.
+ */
+public interface PayD {
+    String getCustCardNo();
+
+    void setCustCardNo(String custCardNo);
+
+    String getCardOwnerName();
+
+    void setCardOwnerName(String cardOwnerName);
+
+    String getCardExpMonthDate();
+
+    void setCardExpMonthDate(String cardExpMonthDate);
+
+    Integer getCVVNo();
+
+    void setCVVNo(Integer cVVNo);
+
+    Double getTotalAmount();
+
+    void setTotalAmount(Double totalAmount);
+}
