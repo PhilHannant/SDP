f07f45fc57eb7da7ac3d21618f51edae5078911c
philhannant <philip.a.hannant@gmail.com>
2016-01-23 16:04:24 +0000

    getters and setters added

---
+++ Exercises/WorkSheet3/src/adaptor/XpayToPayDAdapter.java
@@ -0,0 +1,63 @@
+package adaptor;
+
+/**
+ * Created by philhannant on 23/01/2016.
+ */
+public class XpayToPayDAdapter implements PayD {
+
+    private String custCardNo;
+    private String cardOwnerName;
+    private String cardExpMonthDate;
+    private Integer cVVNo;
+    private Double totalAmount;
+
+    @Override
+    public String getCustCardNo() {
+        return custCardNo;
+    }
+
+    @Override
+    public void setCustCardNo(String custCardNo) {
+        this.custCardNo = custCardNo;
+    }
+
+    @Override
+    public String getCardOwnerName() {
+        return cardOwnerName;
+    }
+
+    @Override
+    public void setCardOwnerName(String cardOwnerName) {
+        this.cardOwnerName = cardOwnerName;
+    }
+
+    @Override
+    public String getCardExpMonthDate() {
+        return getCardExpMonthDate();
+    }
+
+    @Override
+    public void setCardExpMonthDate(String cardExpMonthDate) {
+        this.cardExpMonthDate = cardExpMonthDate;
+    }
+
+    @Override
+    public Integer getCVVNo() {
+        return cVVNo;
+    }
+
+    @Override
+    public void setCVVNo(Integer cVVNo) {
+        this.cVVNo = cVVNo;
+    }
+
+    @Override
+    public Double getTotalAmount() {
+        return totalAmount;
+    }
+
+    @Override
+    public void setTotalAmount(Double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+}
