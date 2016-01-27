08e4a8ef50159707d370570ae3f410e8c148cbe8
philhannant <philip.a.hannant@gmail.com>
2016-01-27 22:50:32 +0000

    added range object

---
+++ Exercises/WorkSheet4/src/ClassesObjects.scala
@@ -0,0 +1,16 @@
+/**
+  * Created by philhannant on 27/01/2016.
+  */
+object ClassesObjects {
+
+  def main(args: Array[String]) {
+    val rn1 = 1 to 10
+    println(rn1.step)
+
+    val rn2 = 1 to 10 by 2
+    print(rn2.step)
+  }
+
+
+
+}
