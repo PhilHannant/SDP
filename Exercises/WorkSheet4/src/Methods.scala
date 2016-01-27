98755a446566f1407a1c290830e9b6e71d4e5053
philhannant <philip.a.hannant@gmail.com>
2016-01-27 22:28:14 +0000

    tests within main

--- Exercises/WorkSheet4/src/Methods.scala
+++ Exercises/WorkSheet4/src/Methods.scala
@@ -6,4 +6,22 @@ object Methods {
   def getSquare(a: Int) = {
     a*a
   }
+
+  def isArg1GreaterThanArg2(a: Double, b: Double){
+
+  }
+
+  def main(args: Array[String]) {
+    val a = getSquare(3)
+    assert(a == 9)
+    val b = getSquare(6)
+    assert(b == 36)
+    val c = getSquare(5)
+    assert(c == 25)
+
+    val t1 = isArg1GreaterThanArg2(4.1, 4.12)
+    assert(false)
+    val t2 = isArg1GreaterThanArg2(2.1, 1.2)
+    assert(true)
+  }
 }
