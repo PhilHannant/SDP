1bb053e511233a49055e67a3014b9be50b17b339
philhannant <philip.a.hannant@gmail.com>
2016-01-27 22:42:18 +0000

    tried to add junit - need to read more

--- Exercises/WorkSheet4/tests/MethodTests.scala
+++ Exercises/WorkSheet4/tests/MethodTests.scala
@@ -1,10 +1,28 @@
-import junit.*
+
+import Methods.getSquare
+import org.junit.Test
+
 
 /**
   * Created by philhannant on 27/01/2016.
   */
 object MethodTests {
 
+  @Test
+  def test1() {
+    val a = getSquare(3)
+    assert(a == 9)
+  }
+  //@Test
+  //def test2 {
+    //val a = getSquare(6)
+    //assert(a == 36)
+  //}
 
+  //@Test
+  //def test3 {
+    //val a = getSquare(5)
+    //assert(a == 25)
+  //}
 
 }
