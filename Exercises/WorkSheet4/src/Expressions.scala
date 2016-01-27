10cb264bbc94533c12d18b2dc566b68493963c21
philhannant <philip.a.hannant@gmail.com>
2016-01-27 20:19:22 +0000

    q1 committed

---
+++ Exercises/WorkSheet4/src/Expressions.scala
@@ -0,0 +1,14 @@
+/**
+  * Created by philhannant on 27/01/2016.
+  */
+object Expressions {
+
+  def main(args: Array[String]) {
+    val sky = "sunny"
+    val temp = 90
+    var niceDay = {
+      if (sky == "sunny" && temp > 80) true
+      else false
+    }
+  }
+}
--- Exercises/WorkSheet4/src/REPL.scala
+++
@@ -1,8 +0,0 @@
-/**
-  * Created by philhannant on 27/01/2016.
-  */
-class REPL {
-
-  def
-
-}
