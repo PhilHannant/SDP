950921982a9804d5d9d1515812cc640cf35f781c
philhannant <philip.a.hannant@gmail.com>
2016-01-27 22:17:07 +0000

    added methods and tests

---
+++ Exercises/WorkSheet4/src/Methods.scala
@@ -0,0 +1,9 @@
+/**
+  * Created by philhannant on 27/01/2016.
+  */
+object Methods {
+
+  def getSquare(a: Int) = {
+    a*a
+  }
+}
---
+++ Exercises/WorkSheet4/tests/MethodTests.scala
@@ -0,0 +1,10 @@
+import junit.*
+
+/**
+  * Created by philhannant on 27/01/2016.
+  */
+object MethodTests {
+
+
+
+}
