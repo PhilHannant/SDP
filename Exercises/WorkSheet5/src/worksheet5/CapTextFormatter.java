a4f7381494dc00bd714fba3ae67ab1277f1967e4
philhannant <philip.a.hannant@gmail.com>
2016-02-04 19:49:44 +0000

    methods addded

--- Exercises/WorkSheet5/src/worksheet5/CapTextFormatter.java
+++ Exercises/WorkSheet5/src/worksheet5/CapTextFormatter.java
@@ -8,6 +8,7 @@ public class CapTextFormatter implements TextFormatter{
 
     @Override
     public void format(String text) {
+        System.out.println(text.toUpperCase());
 
     }
 }
--- Exercises/WorkSheet5/src/worksheet5/LowerTextFormatter.java
+++ Exercises/WorkSheet5/src/worksheet5/LowerTextFormatter.java
@@ -8,6 +8,7 @@ public class LowerTextFormatter implements TextFormatter {
 
     @Override
     public void format(String text) {
+        System.out.println(text.toUpperCase());
 
     }
 }
