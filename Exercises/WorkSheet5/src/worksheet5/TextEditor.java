8dd1f50322bcb9d4c2ed96b3ae0e15f0c95d8439
philhannant <philip.a.hannant@gmail.com>
2016-02-04 20:39:38 +0000

    texteditor started

--- Exercises/WorkSheet5/src/worksheet5/TextEditor.java
+++ Exercises/WorkSheet5/src/worksheet5/TextEditor.java
@@ -3,7 +3,15 @@ package worksheet5;
 /**
  * Created by philhannant on 04/02/2016.
  */
-public interface TextEditor {
+public class TextEditor {
 
-    void publishText(String text);
+   private final TextFormatter textFormatter;
+
+
+   public TextEditor(TextFormatter textFormatter){
+       this.textFormatter = textFormatter
+   }
+
+
+   public void publishText(String text);
 }
