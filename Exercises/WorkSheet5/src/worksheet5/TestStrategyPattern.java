9ab0dc5b114d6f13d9ae524e6973c853a97fd9d7
philhannant <philip.a.hannant@gmail.com>
2016-02-04 19:26:08 +0000

    Added text script

---
+++ Exercises/WorkSheet5/src/worksheet5/TestStrategyPattern.java
@@ -0,0 +1,16 @@
+package worksheet5;
+
+/**
+ * Created by philhannant on 04/02/2016.
+ */
+public class TestStrategyPattern {
+    public static void main(String[] args) {
+        TextFormatter formatter = new CapTextFormatter();
+        TextEditor editor = new TextEditor(formatter);
+        editor.publishText("Testing text in caps formatter");
+        formatter = new LowerTextFormatter();
+        editor = new TextEditor(formatter);
+        editor.publishText("Testing text in lower formatter");
+    }
+
+}
