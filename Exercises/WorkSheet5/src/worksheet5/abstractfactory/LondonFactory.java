c357d0df8528ee0ed3008d56ab9a6278b9abb993
philhannant <philip.a.hannant@gmail.com>
2016-02-05 23:43:28 +0000

    added LondonFactory

---
+++ Exercises/WorkSheet5/src/worksheet5/abstractfactory/LondonFactory.java
@@ -0,0 +1,13 @@
+package worksheet5.abstractfactory;
+
+import scala.reflect.internal.Types;
+
+/**
+ * Created by philhannant on 05/02/2016.
+ */
+public class LondonFactory implements AbstractParserFactory {
+    @Override
+    public XMLParser getParserInstance(String parserType) {
+        return null;
+    }
+}
