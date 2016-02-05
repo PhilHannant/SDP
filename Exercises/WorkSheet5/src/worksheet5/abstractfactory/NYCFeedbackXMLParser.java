fa1ed4590439e2de00a90370dcb5e6066c469117
philhannant <philip.a.hannant@gmail.com>
2016-02-05 23:28:56 +0000

    name changed

--- Exercises/WorkSheet5/src/worksheet5/abstractfactory/NYCFeedbackXMLParser.java
+++ Exercises/WorkSheet5/src/worksheet5/abstractfactory/NYCFeedbackXMLParser.java
@@ -3,7 +3,7 @@ package worksheet5.abstractfactory;
 /**
  * Created by philhannant on 05/02/2016.
  */
-public class NYCFeedbackXML implements XMLParser {
+public class NYCFeedbackXMLParser implements XMLParser {
     @Override
     public String parse() {
         return "NYC XML Feedback Message...";
