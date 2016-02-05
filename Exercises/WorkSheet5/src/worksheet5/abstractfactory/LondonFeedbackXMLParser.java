7d5951a4c260866c23e79b988c4899687ed4ef36
philhannant <philip.a.hannant@gmail.com>
2016-02-05 23:31:18 +0000

    initial commit

---
+++ Exercises/WorkSheet5/src/worksheet5/abstractfactory/LondonFeedbackXMLParser.java
@@ -0,0 +1,11 @@
+package worksheet5.abstractfactory;
+
+/**
+ * Created by philhannant on 05/02/2016.
+ */
+public class LondonFeedbackXMLParser implements XMLParser {
+    @Override
+    public String parse() {
+        return null;
+    }
+}
