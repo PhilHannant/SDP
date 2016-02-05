7e10772caf58e06066f5bb684c6fccdae85c85c3
philhannant <philip.a.hannant@gmail.com>
2016-02-05 22:40:25 +0000

    initial commit

---
+++ Exercises/WorkSheet5/src/worksheet5/abstractfactory/ParserFactoryProducer.java
@@ -0,0 +1,13 @@
+package worksheet5.abstractfactory;
+
+import com.sun.deploy.xml.XMLParser;
+
+/**
+ * Created by philhannant on 05/02/2016.
+ */
+public class ParserFactoryProducer {
+
+    public XMLParser getFactory(String parserType) {
+        return null;
+    }
+}
