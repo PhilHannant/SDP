27a36dc349b761cfe10d9ffec2d25d7fc33b86ee
philhannant <philip.a.hannant@gmail.com>
2016-02-06 15:33:45 +0000

    Londonfactory code addded

--- Exercises/WorkSheet5/src/worksheet5/abstractfactory/LondonFactory.java
+++ Exercises/WorkSheet5/src/worksheet5/abstractfactory/LondonFactory.java
@@ -8,6 +8,16 @@ import scala.reflect.internal.Types;
 public class LondonFactory implements AbstractParserFactory {
     @Override
     public XMLParser getParserInstance(String parserType) {
-        return null;
+        if(parserType.equalsIgnoreCase("LondonError")){
+            return new LondonErrorXMLParser();
+        } else if (parserType.equalsIgnoreCase("LondonFeedback")){
+            return new LondonFeedbackXMLParser()
+        } else if (parserType.equalsIgnoreCase("LondonOrder")){
+            return new LondonOrderXMLParser();
+        } else if (parserType.equalsIgnoreCase("LondonResponse")){
+            return new LondonResponseXMLParser();
+        } else {
+            System.out.println("Parser Type not recognised");
+        }
     }
 }
