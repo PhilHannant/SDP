e9322f80eb01929795df0562fc1edb33d7e164cc
philhannant <philip.a.hannant@gmail.com>
2016-02-08 21:21:52 +0000

    basic code

--- Exercises/WorkSheet5/src/worksheet5/builder/CarDirector.java
+++ Exercises/WorkSheet5/src/worksheet5/builder/CarDirector.java
@@ -4,4 +4,14 @@ package worksheet5.builder;
  * Created by philhannant on 08/02/2016.
  */
 public class CarDirector {
+
+    private CarDirector carDirector;
+
+    public CarDirector(CarDirector carDirector){
+        this.carDirector = carDirector;
+    }
+
+    public Car build(){
+        return null;
+    }
 }
