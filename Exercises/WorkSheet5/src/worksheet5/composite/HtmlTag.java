ab60c4b80e4e0a03f1b2cc0ef8dfe1e888a03b65
philhannant <philip.a.hannant@gmail.com>
2016-02-09 14:31:15 +0000

    initial commit

--- Exercises/WorkSheet5/src/worksheet5/composite/HtmlTag.java
+++ Exercises/WorkSheet5/src/worksheet5/composite/HtmlTag.java
@@ -8,7 +8,7 @@ import java.util.List;
 public abstract class HtmlTag {
 
     public abstract String getTagName();
-    
+
     public abstract void setStartTag(String tag);
 
     public abstract void setEndTag(String tag);
