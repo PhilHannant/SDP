8e8944f350d4a0ab9dc17258ae551d6c262ddf22
philhannant <philip.a.hannant@gmail.com>
2016-02-09 14:30:52 +0000

    initial commit

---
+++ Exercises/WorkSheet5/src/worksheet5/composite/HtmlTag.java
@@ -0,0 +1,33 @@
+package worksheet5.composite;
+
+import java.util.List;
+
+/**
+ * Created by philhannant on 09/02/2016.
+ */
+public abstract class HtmlTag {
+
+    public abstract String getTagName();
+    
+    public abstract void setStartTag(String tag);
+
+    public abstract void setEndTag(String tag);
+
+    public void setTagBody(String tagBody) {
+        throw new UnsupportedOperationException("Current operation is not support - for this object");
+    }
+
+    public void addChildTag(HtmlTag htmlTag) {
+        throw new UnsupportedOperationException("Current operation is not support - for this object");
+    }
+
+    public void removeChildTag(HtmlTag htmlTag) {
+        throw new UnsupportedOperationException("Current operation is not support - for this object");
+    }
+
+    public List<HtmlTag> getChildren() {
+        throw new UnsupportedOperationException("Current operation is not support - for this object");
+    }
+
+    public abstract void generateHtml();
+}
