b8864d4ecd4de916f9e3012e1c1388338147cd0b
philhannant <philip.a.hannant@gmail.com>
2016-02-09 11:42:15 +0000

    testign code added and successfully run

--- Exercises/WorkSheet5/src/worksheet5/facadePattern/TestscheduleServer.java
+++ Exercises/WorkSheet5/src/worksheet5/facadePattern/TestscheduleServer.java
@@ -7,22 +7,13 @@ public class TestscheduleServer {
 
     public static void main(String[] args){
 
-        ScheduleServer scheduleServer = new ScheduleServer();
+        StartStopServer sss = new StartStopServer();
 
-        scheduleServer.startBooting();
-        scheduleServer.readSystemConfigFile();
-        scheduleServer.init();
-        scheduleServer.initializeContext();
-        scheduleServer.initializeListeners();
-        scheduleServer.createSystemObjects();
-        System.out.println("Start working......");
-        System.out.println("After work done.........");
+        sss.startServer();
+        sss.stopServer();
 
-        scheduleServer.releaseProcesses();
-        scheduleServer.destory();
-        scheduleServer.destroySystemObjects();
-        scheduleServer.destoryListeners();
-        scheduleServer.destoryContext();
-        scheduleServer.shutdown();
+
+
+
     }
 }
