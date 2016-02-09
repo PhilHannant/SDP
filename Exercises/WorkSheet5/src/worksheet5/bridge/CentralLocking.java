90eedea811c2037c85f6067200d3e29da7697c71
philhannant <philip.a.hannant@gmail.com>
2016-02-09 12:26:07 +0000

    central locking added

---
+++ Exercises/WorkSheet5/src/worksheet5/bridge/CentralLocking.java
@@ -0,0 +1,23 @@
+package worksheet5.bridge;
+
+/**
+ * Created by philhannant on 09/02/2016.
+ */
+public class CentralLocking implements Product {
+
+    private String productName;
+
+    public CentralLocking(String productName){
+        this.productName = productName;
+    }
+
+    @Override
+    public String productName() {
+        return productName;
+    }
+
+    @Override
+    public void produce() {
+        System.out.println("Producing Gear Locking System");
+    }
+}
