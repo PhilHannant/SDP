225e2301e9b8e51f772d308e043bdd086218f160
philhannant <philip.a.hannant@gmail.com>
2016-02-10 15:45:29 +0000

    added some code

--- Exercises/WorkSheet5/src/worksheet5/composite/HtmlParentElement.java
+++ Exercises/WorkSheet5/src/worksheet5/composite/HtmlParentElement.java
@@ -1,22 +1,38 @@
 package worksheet5.composite;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by philhannant on 09/02/2016.
  */
+
+
+
 public class HtmlParentElement extends HtmlTag {
+
+    private String name;
+    private String endTag = "";
+    private String startTag = "";
+    private List<HtmlTag>childTags = new ArrayList<>();
+
+    public HtmlParentElement(String name){
+        this.name = name;
+    }
+
     @Override
     public String getTagName() {
-        return null;
+        return name;
     }
 
     @Override
     public void setStartTag(String tag) {
-
+        this.startTag = tag;
     }
 
     @Override
     public void setEndTag(String tag) {
-
+        this.endTag = tag;
     }
 
     @Override
