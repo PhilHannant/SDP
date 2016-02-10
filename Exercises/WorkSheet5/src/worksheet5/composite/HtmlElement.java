31a9e0b0b3262fd4edeb8b39246b7dc392a7ecd2
philhannant <philip.a.hannant@gmail.com>
2016-02-10 15:19:29 +0000

    started

--- Exercises/WorkSheet5/src/worksheet5/composite/HtmlElement.java
+++ Exercises/WorkSheet5/src/worksheet5/composite/HtmlElement.java
@@ -3,5 +3,34 @@ package worksheet5.composite;
 /**
  * Created by philhannant on 09/02/2016.
  */
-public class HtmlElement {
+public class HtmlElement extends HtmlTag {
+
+    private String name;
+    private String endTag;
+    private String startTag;
+
+    public HtmlElement(String name){
+        this.name = name;
+    }
+
+
+    @Override
+    public String getTagName() {
+        return null;
+    }
+
+    @Override
+    public void setStartTag(String tag) {
+
+    }
+
+    @Override
+    public void setEndTag(String tag) {
+
+    }
+
+    @Override
+    public void generateHtml() {
+
+    }
 }
