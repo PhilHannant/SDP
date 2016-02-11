0494b312392374a1b79dd3ca66e185d2e2550f63
philhannant <philip.a.hannant@gmail.com>
2016-02-11 20:33:31 +0000

    started 3

---
+++ Exercises/WorkSheet6/src/classargs/TestArgs.scala
@@ -0,0 +1,24 @@
+package classargs
+
+import atomic.AtomicTest._
+
+/**
+  * Created by philhannant on 11/02/2016.
+  */
+object TestArgs extends App {
+
+    val family1 = new Family("Mum", "Dad", "Sally", "Dick")
+    family1.familySize() is 4
+
+    val family2 = new Family("Dad", "Mom", "Harry")
+    family2.familySize() is 3
+}
+
+class Family(args: String*){
+
+
+  def familySize(family: List[String]) = {
+    family.size
+  }
+
+}
