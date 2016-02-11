7f8b87e255c9d262f6a055ea0a12db7496d08c8c
philhannant <philip.a.hannant@gmail.com>
2016-02-11 20:55:41 +0000

    hmmmm

---
+++ Exercises/WorkSheet6/src/classargs/Squarer.scala
@@ -0,0 +1,18 @@
+package classargs
+/**
+  * Created by philhannant on 11/02/2016.
+  */
+object Squarer {
+  squareThem(2) is 4
+  squareThem(2, 4) is 20
+  squareThem(1, 2, 4) is 21
+}
+
+class squareStuff(){
+
+  def squareThem(args: Number*) = {
+
+    
+    }
+  }
+}
