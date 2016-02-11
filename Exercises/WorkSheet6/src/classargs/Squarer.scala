82b3838be84a49a87051fcef6785488fc3872b79
philhannant <philip.a.hannant@gmail.com>
2016-02-11 21:02:58 +0000

    added import and moved method into test object

--- Exercises/WorkSheet6/src/classargs/Squarer.scala
+++ Exercises/WorkSheet6/src/classargs/Squarer.scala
@@ -1,4 +1,7 @@
 package classargs
+
+import atomic.AtomicTest._
+
 /**
   * Created by philhannant on 11/02/2016.
   */
@@ -6,13 +9,12 @@ object Squarer {
   squareThem(2) is 4
   squareThem(2, 4) is 20
   squareThem(1, 2, 4) is 21
-}
 
-class squareStuff(){
 
   def squareThem(args: Number*) = {
 
-    
-    }
+
+
+
   }
 }
