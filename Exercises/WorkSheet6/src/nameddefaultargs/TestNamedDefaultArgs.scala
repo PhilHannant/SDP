44c7a20c9cfd73e54fd7e895725f1868993153ce
philhannant <philip.a.hannant@gmail.com>
2016-02-12 12:25:44 +0000

    7 complete

--- Exercises/WorkSheet6/src/nameddefaultargs/TestNamedDefaultArgs.scala
+++ Exercises/WorkSheet6/src/nameddefaultargs/TestNamedDefaultArgs.scala
@@ -10,8 +10,11 @@ object TestNamedDefaultArgs extends App {
   t.hours is 5
   t.minutes is 30
 
+  val t2 = new SimpleTime2(hours=10)
+  t2.hours is 10
+  t2.minutes is 0
 }
 
-case class SimpleTime(hours: Int, minutes: Int){
+case class SimpleTime(hours: Int, minutes: Int){}
 
-}
+case class SimpleTime2(hours: Int, minutes: Int = 0){}
