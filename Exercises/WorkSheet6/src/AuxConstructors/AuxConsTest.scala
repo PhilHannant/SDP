a37e98397fbec2f95f52051163475d2ff5b99460
philhannant <philip.a.hannant@gmail.com>
2016-02-13 14:57:37 +0000

    added val call before this and received error expecting this instead of val

--- Exercises/WorkSheet6/src/AuxConstructors/AuxConsTest.scala
+++ Exercises/WorkSheet6/src/AuxConstructors/AuxConsTest.scala
@@ -1,15 +1,20 @@
 package AuxConstructors
 
+import atomic.AtomicTest._
 /**
   * Created by philhannant on 13/02/2016.
   */
-object AuxConsTest {
+object AuxConsTest extends App{
+
+  val washer = new ClothesWasher()
+  washer.capacity is 0
 
 }
 
 class ClothesWasher(val modelName: String, val capacity: Double){
 
   def this() = {
+    val something: String = "hello"
     this("", 0)
   }
 
