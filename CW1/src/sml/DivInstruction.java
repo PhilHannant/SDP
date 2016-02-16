bacb378c8ba5d9a700a05982812c87dde741ed00
philhannant <philip.a.hannant@gmail.com>
2016-02-16 21:12:20 +0000

    overloaded constructor added

--- CW1/src/sml/DivInstruction.java
+++ CW1/src/sml/DivInstruction.java
@@ -5,11 +5,21 @@ package sml;
  */
 public class DivInstruction extends Instruction {
 
+    private int result;
+    private int op1;
+    private int op2;
+
 
     public DivInstruction(String l, String op) {
         super(l, op);
     }
 
+    public DivInstruction(String label, int result, int op1, int op2){
+        super(label, "div");
+        this.result = result;
+        this.op1 = op1;
+        this.op2 = op2;
+    }
     @Override
     public void execute(Machine m) {
 
