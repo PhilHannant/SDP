d735d5b38b218560310a0d379eae6af730315bd8
philhannant <philip.a.hannant@gmail.com>
2016-02-16 21:00:50 +0000

    excute code added, tests run successfully

--- CW1/src/sml/MulInstruction.java
+++ CW1/src/sml/MulInstruction.java
@@ -23,6 +23,8 @@ public class MulInstruction extends Instruction {
 
     @Override
     public void execute(Machine m) {
-
+        int value1 = m.getRegisters().getRegister(op1);
+        int value2 = m.getRegisters().getRegister(op2);
+        m.getRegisters().setRegister(result, value1 + value2);
     }
 }
