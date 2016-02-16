ab97d7b64f8aed4c21f510ec3d81462b709d07e3
philhannant <philip.a.hannant@gmail.com>
2016-02-16 10:59:27 +0000

    constructor added

--- CW1/src/sml/MinusInstruction.java
+++ CW1/src/sml/MinusInstruction.java
@@ -4,6 +4,11 @@ package sml;
  * Created by philhannant on 16/02/2016.
  */
 public class MinusInstruction extends Instruction {
+
+    public MinusInstruction(String l, String op) {
+        super(l, op);
+    }
+
     @Override
     public void execute(Machine m) {
 
