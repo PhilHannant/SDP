8232bedf7d40bf0d24ee0746429a6cb951f96ac7
philhannant <philip.a.hannant@gmail.com>
2016-02-16 11:05:09 +0000

    added minus tests

---
+++ CW1/src/Tests/MinusInstructionTest.java
@@ -0,0 +1,14 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by philhannant on 16/02/2016.
+ */
+public class MinusInstructionTest {
+
+    @org.junit.Test
+    public void testExecute() throws Exception {
+
+    }
+}
