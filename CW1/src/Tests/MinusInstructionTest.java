f1d290a61d56d76c9d7ec53250cd53b35f9b944e
philhannant <philip.a.hannant@gmail.com>
2016-02-16 18:05:35 +0000

    tests added and failed

--- CW1/src/Tests/MinusInstructionTest.java
+++ CW1/src/Tests/MinusInstructionTest.java
@@ -1,14 +1,35 @@
 package Tests;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import sml.Machine;
+import sml.MinusInstruction;
+import sml.Registers;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by philhannant on 16/02/2016.
  */
 public class MinusInstructionTest {
+    private MinusInstruction addIns;
+    private Machine m;
 
-    @org.junit.Test
-    public void testExecute() throws Exception {
+    @Before
+    public void construct() {
+        m = new Machine();
+        addIns = new MinusInstruction("f0", 5, 2, 3);
+        m.setRegisters(new Registers());
+        m.getRegisters().setRegister(3, 10);
+        m.getRegisters().setRegister(1, 34);
+        m.getRegisters().setRegister(2, 9);
+    }
+
+    @Test
+    public void testExecute(){
+        addIns.execute(m);
+        assertEquals(34, m.getRegisters().getRegister(1));
 
     }
 }
