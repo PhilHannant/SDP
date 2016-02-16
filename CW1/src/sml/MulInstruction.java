91f58e8e44088291bb871b7bf4d4971d81f2f106
philhannant <philip.a.hannant@gmail.com>
2016-02-16 20:57:47 +0000

    added overloaded constructor and test passes

--- CW1/src/sml/MulInstruction.java
+++ CW1/src/sml/MulInstruction.java
@@ -5,9 +5,20 @@ package sml;
  */
 public class MulInstruction extends Instruction {
 
+    private int result;
+    private int op1;
+    private int op2;
+
 
     public MulInstruction(String l, String op) {
         super(l, op);
+    }
+
+    public MulInstruction(String label, int result, int op1, int op2){
+        super(label, "mul");
+        this.result = result;
+        this.op1 = op1;
+        this.op2 = op2;
     }
 
     @Override
