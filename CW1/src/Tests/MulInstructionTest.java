1096e870d5ede710708ad1ad583576baa960beb2
philhannant <philip.a.hannant@gmail.com>
2016-02-16 20:39:41 +0000

    initial commit

---
+++ CW1/src/Tests/MulInstructionTest.java
@@ -0,0 +1,10 @@
+package Tests;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by philhannant on 16/02/2016.
+ */
+public class MulInstructionTest {
+
+}
---
+++ CW1/src/sml/MulInstruction.java
@@ -0,0 +1,17 @@
+package sml;
+
+/**
+ * Created by philhannant on 16/02/2016.
+ */
+public class MulInstruction extends Instruction {
+
+
+    public MulInstruction(String l, String op) {
+        super(l, op);
+    }
+
+    @Override
+    public void execute(Machine m) {
+
+    }
+}
