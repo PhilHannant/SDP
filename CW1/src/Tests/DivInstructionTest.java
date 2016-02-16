40a4a493c72ca0144840897b749680984ceee91f
philhannant <philip.a.hannant@gmail.com>
2016-02-16 21:10:07 +0000

    construct before added

--- CW1/src/Tests/DivInstructionTest.java
+++ CW1/src/Tests/DivInstructionTest.java
@@ -1,6 +1,10 @@
 package Tests;
 
+import org.junit.Before;
 import org.junit.Test;
+import sml.Machine;
+import sml.DivInstruction;
+import sml.Registers;
 
 import static org.junit.Assert.*;
 
@@ -8,6 +12,20 @@ import static org.junit.Assert.*;
  * Created by philhannant on 16/02/2016.
  */
 public class DivInstructionTest {
+
+
+    private DivInstruction divIns;
+    private Machine m;
+
+    @Before
+    public void construct() {
+        m = new Machine();
+        divIns = new DivInstruction("f0", 5, 2, 3);
+        m.setRegisters(new Registers());
+        m.getRegisters().setRegister(3, 10);
+        m.getRegisters().setRegister(1, 34);
+        m.getRegisters().setRegister(2, 9);
+    }
 
     @Test
     public void testExecute() throws Exception {
