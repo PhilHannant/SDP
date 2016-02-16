431040b4ca9baa33e6bb642bd699184c9d3c7066
philhannant <philip.a.hannant@gmail.com>
2016-02-16 21:03:03 +0000

    initial commit

---
+++ CW1/src/Tests/DivInstructionTest.java
@@ -0,0 +1,16 @@
+package Tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by philhannant on 16/02/2016.
+ */
+public class DivInstructionTest {
+
+    @Test
+    public void testExecute() throws Exception {
+
+    }
+}
---
+++ CW1/src/sml/DivInstruction.java
@@ -0,0 +1,17 @@
+package sml;
+
+/**
+ * Created by philhannant on 16/02/2016.
+ */
+public class DivInstruction extends Instruction {
+
+
+    public DivInstruction(String l, String op) {
+        super(l, op);
+    }
+
+    @Override
+    public void execute(Machine m) {
+
+    }
+}
