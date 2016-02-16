ef80d2212dd152511a256152887168b2bceb1a19
philhannant <philip.a.hannant@gmail.com>
2016-02-16 10:55:28 +0000

    initial commit

---
+++ CW1/src/sml/MinusInstruction.java
@@ -0,0 +1,11 @@
+package sml;
+
+/**
+ * Created by philhannant on 16/02/2016.
+ */
+public class MinusInstruction extends Instruction {
+    @Override
+    public void execute(Machine m) {
+
+    }
+}
