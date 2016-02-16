8c70c04d015901af5fa2909d5a862ffddde27400
philhannant <philip.a.hannant@gmail.com>
2016-02-16 20:55:43 +0000

    added construct as in previous tests

--- CW1/src/Tests/MulInstructionTest.java
+++ CW1/src/Tests/MulInstructionTest.java
@@ -1,5 +1,10 @@
 package Tests;
 
+import org.junit.Before;
+import sml.MulInstruction;
+import sml.Machine;
+import sml.Registers;
+
 import static org.junit.Assert.*;
 
 /**
@@ -7,4 +12,17 @@ import static org.junit.Assert.*;
  */
 public class MulInstructionTest {
 
+
+    private MulInstruction mulIns;
+    private Machine m;
+
+    @Before
+    public void construct() {
+        m = new Machine();
+        mulIns = new MulInstruction("f0", 5, 2, 3);
+        m.setRegisters(new Registers());
+        m.getRegisters().setRegister(3, 10);
+        m.getRegisters().setRegister(1, 34);
+        m.getRegisters().setRegister(2, 9);
+    }
 }
