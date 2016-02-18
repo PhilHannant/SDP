2b1387b2675f43f858562bb7cf57d2a360cbb71f
philhannant <philip.a.hannant@gmail.com>
2016-02-18 14:29:53 +0000

    setUp added, failing due to constructor not existing

--- CW1/src/Tests/OutInstructionTest.java
+++ CW1/src/Tests/OutInstructionTest.java
@@ -1,13 +1,32 @@
 package Tests;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import sml.Machine;
+import sml.OutInstruction;
+import sml.Registers;
+
+
+
 
 /**
  * Created by philhannant on 18/02/2016.
  */
 public class OutInstructionTest {
+
+    private Machine m;
+    private OutInstruction outIns;
+
+    @Before
+    public void setUp(){
+        m = new Machine();
+        outIns = new OutInstruction("f0", 5, 2, 3);
+        m.setRegisters(new Registers());
+        m.getRegisters().setRegister(3, 10);
+        m.getRegisters().setRegister(1, 34);
+        m.getRegisters().setRegister(2, 9);
+    }
 
     @Test
     public void testExecute() throws Exception {
