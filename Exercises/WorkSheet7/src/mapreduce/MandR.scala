19665f1135b731dd0779cf82fd1c0f4343dce76c
philhannant <philip.a.hannant@gmail.com>
2016-02-18 20:06:49 +0000

    added foreach much more complexe

--- Exercises/WorkSheet7/src/mapreduce/MandR.scala
+++ Exercises/WorkSheet7/src/mapreduce/MandR.scala
@@ -10,8 +10,9 @@ object MandR extends App {
   val v1 = Vector(1, 2, 3, 4)
   v1.map(n => n + 1) is Vector(2, 3, 4, 5)
 
+  v1.map(n => n * 11 + 10) is Vector(21, 32, 43, 54)
 
-  val v2 = Vector(1, 2, 3, 4)
-  v2.map(n => n * 11 + 10) is Vector(21, 32, 43, 54)
 
+  val v2 = for (e <- v1) yield e * 11 + 10
+  v2 is Vector(21, 32, 43, 54)
 }
