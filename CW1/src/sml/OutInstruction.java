03ba1696bcf155d7ec3974a6b788f174dc4ef58d
philhannant <philip.a.hannant@gmail.com>
2016-02-18 14:31:23 +0000

    overloaded constructor added

--- CW1/src/sml/OutInstruction.java
+++ CW1/src/sml/OutInstruction.java
@@ -4,8 +4,20 @@ package sml;
  * Created by philhannant on 18/02/2016.
  */
 public class OutInstruction extends Instruction {
+
+    private int result;
+    private int op1;
+    private int op2;
+
     public OutInstruction(String l, String op) {
         super(l, op);
+    }
+
+    public OutInstruction(String label, int result, int op1, int op2){
+        super(label, "out");
+        this.result = result;
+        this.op1 = op1;
+        this.op2 = op2;
     }
 
     @Override
