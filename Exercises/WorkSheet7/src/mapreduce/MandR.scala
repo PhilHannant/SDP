bd0215b0e0844061bcec4b1a3deb3f0dbbe4a584
philhannant <philip.a.hannant@gmail.com>
2016-02-18 20:11:55 +0000

    2 done

--- Exercises/WorkSheet7/src/mapreduce/MandR.scala
+++ Exercises/WorkSheet7/src/mapreduce/MandR.scala
@@ -15,4 +15,13 @@ object MandR extends App {
 
   val v2 = for (e <- v1) yield e * 11 + 10
   v2 is Vector(21, 32, 43, 54)
+
+
+  val v = Vector(1, 2, 3, 4)
+  v.map(n => n + 1) is Vector(2, 3, 4, 5)
+
+  val v3 = for (e <- v) yield e +1
+  v3 is Vector(2, 3, 4, 5)
+
+
 }
