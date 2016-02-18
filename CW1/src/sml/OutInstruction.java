0a53c49f9863afc13c52604fc89e743445253608
philhannant <philip.a.hannant@gmail.com>
2016-02-18 14:47:09 +0000

    fixed more errors

--- CW1/src/sml/OutInstruction.java
+++ CW1/src/sml/OutInstruction.java
@@ -13,11 +13,10 @@ public class OutInstruction extends Instruction {
         super(l, op);
     }
 
-    public OutInstruction(String label, int result, int op1, int op2){
+    public OutInstruction(String label, int result, int op1){
         super(label, "out");
         this.result = result;
         this.op1 = op1;
-        this.op2 = op2;
     }
 
     @Override
