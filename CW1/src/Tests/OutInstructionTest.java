f6fdc96bd396743ae830d0518c01f22daa705a83
philhannant <philip.a.hannant@gmail.com>
2016-02-18 14:27:18 +0000

    initial commit

---
+++ CW1/src/Tests/OutInstructionTest.java
@@ -0,0 +1,16 @@
+package Tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by philhannant on 18/02/2016.
+ */
+public class OutInstructionTest {
+
+    @Test
+    public void testExecute() throws Exception {
+
+    }
+}
---
+++ CW1/src/sml/OutInstruction.java
@@ -0,0 +1,15 @@
+package sml;
+
+/**
+ * Created by philhannant on 18/02/2016.
+ */
+public class OutInstruction extends Instruction {
+    public OutInstruction(String l, String op) {
+        super(l, op);
+    }
+
+    @Override
+    public void execute(Machine m) {
+
+    }
+}
