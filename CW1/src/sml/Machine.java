0429bfb54f9780411894d8303324e3543752fbc8
philhannant <philip.a.hannant@gmail.com>
2016-02-20 09:54:58 +0000

    mul working

--- CW1/src/sml/Machine.java
+++ CW1/src/sml/Machine.java
@@ -33,7 +33,7 @@ public class Machine {
     }
 
     public static void main(String[] args) {
-        String filename = "/Users/philhannant/IdeaProjects/SDP/CW1/src/testSub.sml";
+        String filename = "/Users/philhannant/IdeaProjects/SDP/CW1/src/testMul.sml";
 
 
         Machine m = new Machine();
